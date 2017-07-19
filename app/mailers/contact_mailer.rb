afaaa1a024f44c793183abedd30d87d52c07245f
AlexChi <alexchi1114@gmail.com>
2017-07-19 10:25:08 -0400

    adding sendgrid functionality

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,9 @@
+class ContactMailer < ApplicationMailer
+	default :from => 'do-not-reply@theresaalianell.com'
+
+	def contact_email(contact)
+		@contact = contact
+		mail(:to => 'alexchi1114@gmail.com')
+		:subject => @contact.subject
+	end
+end
