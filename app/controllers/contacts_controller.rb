c4cf5d307b2f9656c89f768e39ac20a5e50c8ce5
AlexChi <alexchi1114@gmail.com>
2017-07-19 10:54:07 -0400

    added flash for message

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,7 +6,7 @@ class ContactsController < ApplicationController
 	def create
 		@contact = Contact.new(contact_params)
     	if @contact.save
-    	  flash[:notice] = "Thank you for your message. We will get back to you shortly."
+    	  flash[:success] = "Thank you for your message. We will get back to you shortly."
           ContactMailer.contact_email(@contact).deliver
     	  redirect_to root_url
     	else
